e58fcd6374c8fef014c6828d0ff12a96c6d6a481
rifqiay <muhammadrifqiay5@gmail.com>
2022-09-15 16:08:45 +0700

    add features login

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Fragment } from "react";
+import Login from "../pages/auth/login/Login";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 };
