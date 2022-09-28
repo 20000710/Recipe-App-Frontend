8f6c8b689be135029ccfc1376d28140a0b2c1f11
mallyaagung <Mallyaagung@gmail.com>
2022-09-28 21:16:36 +0800

    add search page

--- src/pages/recipe/addRecipe.js
+++ src/pages/recipe/addRecipe.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import Footer from "../../components/Footer/index";
+import Footer from "../../components/footer";
 import FormAddRecipe from "../../components/form/formAddRecipe";
 import NavbarMenu from "../../components/navbar/navbarMenu";
 
